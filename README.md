933bf6cfc0268f9c73b617f8c66de7cd18f85b7e
Refined Guides <refinedguides@pm.me>
2024-03-27 00:52:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Form Validation
+
+A simple demo of Constraint Validation API using HTML5 and Javascript.
+
+### Features Covered
+
+- [x] HTML validation
+- [x] CSS pseudo-classes
+- [x] Tooltip to error text
+- [x] Custom validation
+
+### Screenshot
+
+![Form Validation](https://raw.githubusercontent.com/refinedguides/form-validation/main/screenshot.png)
---
+++ script.js
@@ -0,0 +1,99 @@
+const form = document.querySelector("form");
+const inputs = form.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("change", () => {
+    validateInput(input);
+  });
+});
+
+// disable browsers validation
+form.noValidate = true;
+
+// handle form submit event
+form.addEventListener("submit", (e) => {
+  if (!form.checkValidity()) {
+    e.preventDefault(); // prevent form submission
+
+    inputs.forEach(validateInput);
+  } else {
+    alert("form submitted successfully!");
+
+    // Send data to the backend
+  }
+});
+
+const validateInput = (input) => {
+  let errorText = input.nextElementSibling;
+  if (!errorText) {
+    errorText = document.createElement("small");
+    errorText.className = "error-text";
+    input.insertAdjacentElement("afterend", errorText);
+  }
+
+  input.setCustomValidity("");
+
+  checkValidationRules(input);
+
+  const isValid = input.checkValidity();
+
+  if (input.validity.patternMismatch) {
+    input.setCustomValidity(input.title);
+  }
+
+  input.classList.toggle("invalid", !isValid);
+
+  errorText.textContent = input.validationMessage;
+};
+
+const rules = {
+  username: {
+    disallowedWords: {
+      words: ["offensive", "inappropriate"],
+      error: "Username is invalid. Please choose another one.",
+    },
+  },
+  "password-conf": {
+    match: {
+      target: "password",
+      error: "Passwords do not match",
+    },
+  },
+};
+
+const checkValidationRules = (input) => {
+  // skip inputs with no custom validation
+  if (!rules[input.id]) return;
+
+  for (const rule in rules[input.id]) {
+    applyValidationRule(rule, input);
+  }
+};
+
+const applyValidationRule = (rule, input) => {
+  const ruleset = rules[input.id][rule];
+
+  let error = ruleset.error;
+
+  switch (rule) {
+    case "match":
+      const matchInput = document.getElementById(ruleset.target);
+      if (!matchInput) {
+        throw new Error(`${ruleset.target} field is not found`);
+      }
+
+      if (input.value !== matchInput.value) {
+        input.setCustomValidity(error);
+      }
+
+      break;
+    case "disallowedWords":
+      // join the banned words into string and make it case insensitive
+      const words = new RegExp(ruleset.words.join("|"), "i");
+      if (words.test(input.value)) {
+        input.setCustomValidity(error);
+      }
+
+      break;
+  }
+};
